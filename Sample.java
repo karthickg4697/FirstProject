78443a9fd76983fbc583b47bebe3ec4b1a300ce8
karthickg4697 <136255965+karthickg4697@users.noreply.github.com>
2023-06-17 14:50:57 +0530

    Update Sample.java
    Changed a to 0

--- Sample.java
+++ Sample.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 		
-		int a = 200;
+		int a = 0
 
         // Creates a reader instance which takes
         // input from standard input - keyboard
