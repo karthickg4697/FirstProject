931e131e47dcb9d360969bfa82bf19bf6fcc5716
Prabhu Raj <136254544+PrabhuRaj2001@users.noreply.github.com>
2023-06-17 23:08:58 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -10,7 +10,7 @@ public class HelloWorld {
         // input from standard input - keyboard
         //Scanner reader = new Scanner(System.in);
         System.out.print("Enter a number: ");
-
+	System.out.println("hello");
         // nextInt() reads the next integer from the keyboard
         //int number = reader.nextInt();
 
